b8f2d529d23bcd927eaa131f7c8f383c11ea1bf6
Liz Borchardt <zardra@silvana.gateway.2wire.net>
2013-10-13 18:56:54 -0700

    count_unique done

--- skills2/skills.py
+++ skills2/skills.py
@@ -1,11 +1,30 @@
 # Write a function that takes an iterable (something you can loop through, ie: string, list, or tuple) and produces a dictionary with all distinct elements as the keys, and the number of each element as the value
 def count_unique(some_iterable):
-    return {}
+    d = {}
+
+    for item in some_iterable:
+        d[item] = d.get(item, 0) + 1
+
+    return d
 
 # Given two lists, (without using the keyword 'in' or the method 'index') return a list of all common items shared between both lists
 def common_items(list1, list2):
+    common_list = []
+    i = 0
+
+    while i < len(common_items):
+
     return []
 
 # Given two lists, (without using the keyword 'in' or the method 'index') return a list of all common items shared between both lists. This time, use a dictionary as part of your solution.
 def common_items2(list1, list2):
-    return []
+    common_list = []
+    i = 0
+
+    total_list = list1 + list2
+    d = count_unique(long_list)
+
+    
+
+    return common_list
+
